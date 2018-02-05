278189bf06b3f7ecef35ffd33aede65d6c5ebc89
Romano Zvjerković <romanozvj@gmail.com>
2018-02-05 20:36:13 +0100

    komit

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -1,5 +1,18 @@
-var cssClass = (function() {
-  'use strict';
+var cssClass = (function () {
+  'use strict'
 
-  // code goes here
-})();
+  return {
+    add: function(element, class) {
+      element.classList.add(class);
+    }
+    remove: function(element, class) {
+      element.classList.remove(class)
+    }
+    toggle: function(element, class) {
+      element.classList.toggle(class);
+    }
+    has: function(element, class) {
+      element.classList.contains(class);
+    }
+  }
+})()
