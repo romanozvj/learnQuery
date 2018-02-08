9d100b7a6fd4aeaafa0b54bdf9dbe3967c191a36
Romano Zvjerković <romanozvj@gmail.com>
2018-02-08 15:32:22 +0100

    dom manipulation

--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,28 @@
-var dom = function(){
-  // code goes here
-};
+var dom = function () {
+}
+
+dom.remove = function (element) {
+  let elementExists = document.getElementById(element.id);
+  if (!elementExists) return;
+  element.parentNode.removeChild(element);
+}
+dom.append = function (targetElement, element) {
+  targetElement.appendChild(element);
+}
+dom.prepend = function (targetElement, element) {
+  targetElement.prepend(element);
+}
+dom.before = function (targetElement, element) {
+  targetElement.parentNode.insertBefore(element, targetElement);
+}
+dom.after = function (targetElement, element) {
+  try {
+    targetElement.parentNode.insertBefore(element, targetElement.nextSibling);
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+dom.val = function (element) {
+  return element.value;
+}
