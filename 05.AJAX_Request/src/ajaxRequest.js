e9057ff344b5b1d1aad5d0e4753c97c4278d7095
Romano Zvjerković <romanozvj@gmail.com>
2018-02-13 13:29:31 +0100

    ajaxRequest

--- 05.AJAX_Request/src/ajaxRequest.js
+++ 05.AJAX_Request/src/ajaxRequest.js
@@ -1,4 +1,24 @@
 function ajaxReq() {
   'use strict';
-  // code goes here
+  let myArguments = arguments;
+  console.log(myArguments);
+  let xhttp = new XMLHttpRequest;
+  let getOrPost = "GET";
+  if (myArguments[1].method) { getOrPost = myArguments[1].method; }
+  xhttp.onreadystatechange = function () {
+    console.log(xhttp);
+    if (xhttp.readyState === 4) {
+      if (xhttp.status === 200) {
+        let myData = JSON.parse(xhttp.responseText);
+        myArguments[1].success.call(myArguments[1].context, myData, xhttp.status, xhttp);
+      }
+      else {
+        myArguments[1].failure.call(myArguments[1].context, xhttp, xhttp.status, xhttp.responseText);
+      }
+      
+      myArguments[1].complete.call(myArguments[1].context, xhttp, xhttp.status);
+    }
+  }
+  xhttp.open(getOrPost, myArguments[0], true);
+  xhttp.send();
 }
