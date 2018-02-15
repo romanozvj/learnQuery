e857fcabfc10c05028a5fe40bedea303897f1713
Romano Zvjerković <romanozvj@gmail.com>
2018-02-15 19:01:32 +0100

    event listeners

--- 06.event_listeners/src/eventListeners.js
+++ 06.event_listeners/src/eventListeners.js
@@ -1,4 +1,14 @@
 var eventListener = (function() {
   'use strict';
-  //code goes here
+  let counter = 0;
+  return {
+    on: function () {
+      //console.log(arguments);
+      let selectedElement = arguments[0];
+      let event = arguments[1];
+      let oneCall = Object.assign({}, arguments[2]);
+      let twoCall = Object.assign({}, arguments[2]);
+      selectedElement.addEventListener(event, callback);
+    }
+  }
 })();
